be4916c521f3c678b36b49c330566f10a505cf22
Hammad Akram <115222833+979hammad@users.noreply.github.com>
2024-08-15 15:47:42 +0500

    Update userFunc.js

--- server/controllers/userFunc.js
+++ server/controllers/userFunc.js
@@ -22,21 +22,21 @@ const signUp = async (req, res) => {
             throw new ExpressError(400, "Email can't exceed 100 digits");
         }
 
-        const checkUserSql = 'SELECT * FROM users WHERE email = ?';
-        const [rows] = await req.db.query(checkUserSql, [email]);
+        const sql = 'SELECT * FROM users WHERE email = ?';
+        const [data] = await req.db.query(sql, [email]);
 
-        if (rows.length > 0) {
+        if (data.length > 0) {
             throw new ExpressError(404, "Email already registered kindly login");
         }
         if (password === cPassword) {
             const salt = await bcrypt.genSalt();
             const hashedPassword = await bcrypt.hash(password, salt);
 
-            const insertUserSql = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
-            const [result] = await req.db.query(insertUserSql, [name, email, hashedPassword]);
+            const sql2 = 'INSERT INTO users (name, email, password) VALUES (?, ?, ?)';
+            const [data] = await req.db.query(sql2, [name, email, hashedPassword]);
 
             const newUser = {
-                id: result.insertId,
+                id: data.insertId,
                 name,
                 email,
             };
@@ -60,10 +60,10 @@ const signin = async (req, res) => {
         throw new ExpressError(400, "Kindly enter all required fields")
     } else {
         const sql = "SELECT * FROM users WHERE email = ?";
-        const [rows] = await req.db.query(sql, [email]);
+        const [data] = await req.db.query(sql, [email]);
     
-        if (rows.length > 0) {
-            const userFound = rows[0];
+        if (data.length > 0) {
+            const userFound = data[0];
 
             const passwordMatched = await bcrypt.compare(password, userFound.password);
             if (passwordMatched) {
@@ -88,7 +88,6 @@ const signin = async (req, res) => {
 }
 
 const myProfile = async(req, res) => {
-    
     res.status(200).json({
         user : req.user
     })
